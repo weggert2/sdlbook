f83012adeec2654668ae252eece0f9abbf99f59b
Bill Eggert <bill@outpost.space>
2023-03-06 19:11:37 -0800

    Displaying a black screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,42 @@
  * @Last Modified by: Bill Eggert
  */
 
-#include <cstdio>
+#include <SDL2/SDL.h>
+
+SDL_Window *g_pWindow = nullptr;
+SDL_Renderer *g_pRenderer = 0;
 
 int main()
 {
-    printf("Hello!\n");
+    // initialize.
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        // Failure.
+        return EXIT_FAILURE;
+    }
+
+    // title, x, y, width, height, flags
+    g_pWindow = SDL_CreateWindow(
+        "Hello SDL",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_SHOWN);
+
+    // window, index, flags.
+    g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+
+    // Sets the color used for drawing operations (rect, line, clear)
+    // renderer, r, g, b, alpha.
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+    // Clear the rendering target. Uses the color set above.
+    SDL_RenderClear(g_pRenderer);
+
+    // Update the screen with any rendering performed since the previous call.
+    SDL_RenderPresent(g_pRenderer);
+
+    SDL_Delay(5000); // ms -- 5 second delay
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
